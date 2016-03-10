680491d5bfc1b1b578b5364f9418ac3d740df808
AryanSingh <aryan@iitgn.ac.in>
2016-03-10 22:23:33 +0530

    background transition color and all the text and buttons added

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -1,14 +1,20 @@
 
-
+function getRandomColor () {
+  var hex = Math.floor(Math.random() * 0xFFFFFF);
+  return "#" + ("000000" + hex.toString(16)).substr(-6);
+}
 $(document).ready(function(){
   $("#new-quote").on("click",function(){
-    $.getJSON("http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1",function(json){
+    $.getJSON("http://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=jsonp&lang=en&jsonp=?",function(json){
+      // json.header("Access-Control-Allow-Origin", "*");
       json = JSON.stringify(json);
-      console.log(json);
+      // console.log(json);
       json = JSON.parse(json);
       console.log(json);
-      console.log(json.contents.quotes[0].quote);
-      $(".quote-text").html(json.contents.quotes[0].quote);
+      var htmlauth = "- ";
+      htmlauth += json.quoteAuthor;
+      $("#text").html(json.quoteText);
+      $("#author").html(htmlauth);
       // console.log(json.contents);
       // var keys = Object.keys(json);
       // console.log(keys);
@@ -30,5 +36,8 @@ $(document).ready(function(){
       
       // $(".quote-text").html(html);
     });
+    var col = getRandomColor();
+    $("body").css("background-color",col);
+    $(".quote").css("color",col);
   });
 });
